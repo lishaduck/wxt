deabc4abc54632d0b49dbcc348df94889f246722
Eli <88557639+lishaduck@users.noreply.github.com>
2025-10-24 10:56:35 -0500

    chore: cleanup esm
    As far as I could tell, the original reasons are no longer reproducible.

--- packages/wxt/src/core/runners/index.ts
+++ packages/wxt/src/core/runners/index.ts
@@ -3,13 +3,14 @@ import { createWslRunner } from './wsl';
 import { createWebExtRunner } from './web-ext';
 import { createSafariRunner } from './safari';
 import { createManualRunner } from './manual';
-import { isWsl } from '../utils/wsl';
+
 import { wxt } from '../wxt';
+import isWsl from 'is-wsl';
 
 export async function createExtensionRunner(): Promise<ExtensionRunner> {
   if (wxt.config.browser === 'safari') return createSafariRunner();
 
-  if (await isWsl()) return createWslRunner();
+  if (isWsl) return createWslRunner();
   if (wxt.config.runnerConfig.config?.disabled) return createManualRunner();
 
   return createWebExtRunner();
